53e69dda4e35ee2ada005b99cdf113c1bd31ddd4
codekuangben <kuangben2001@163.com>
2018-07-30 17:32:21 +0800

    aaa

--- Test/TestLibs/AppDev/Libs/Tools/UtilSysLibWrap.php
+++ Test/TestLibs/AppDev/Libs/Tools/UtilSysLibWrap.php
@@ -10,43 +10,43 @@ class UtilSysLibWrap
 	public const TRUE = "true";
 	public const FALSE = "false";
 
-	public static FAKE_POS = new Vector3(-1000, 0, -1000);  // 默认隐藏到这个位置
-	public const ASSETBUNDLES = "AssetBundles";
+	//public static FAKE_POS = new Vector3(-1000, 0, -1000);  // 默认隐藏到这个位置
+
 	public const CR_LF = "\n";       // 回车换行， Mac 下面即使写入 "\r\n"，读取出来后，也只有 "\n"，因此这里 Windows 下也只写入 "\n"，而不是 "\r\n"
 	public const SEPARATOR = "=";    // 分隔符
 
 	// 剔除字符串末尾的空格
-	public static trimEndSpace(ref string str)
+	public static function trimEndSpace($str)
 	{
-		str.TrimEnd('\0');
+	    $str.TrimEnd('\0');
 	}
 
 	// 判断两个 Object 地址是否相等
-	public static function isAddressEqual(a, b)
+	public static function isAddressEqual($a, $b)
 	{
-		return a === b;
+		return $a === $b;
 	}
 
 	// 判断两个 Object 内容是否相等
-	public static function isObjectEqual(System.Object a, System.Object b)
+	public static function isObjectEqual($a, $b)
 	{
-		return a == b;
+		return $a == $b;
 	}
 
 	// 判断两个函数是否相等，不能使用 isAddressEqual 判断函数是否相等
-	public static bool isDelegateEqual(ref MAction<IDispatchObject> a, ref MAction<IDispatchObject> b)
+	public static function isDelegateEqual($a, $b)
 	{
-		return a == b;
+		return $a == $b;
 	}
 
 	// 判断向量是否相等
-	public static bool isVectorEqual(Vector3 lhv, Vector3 rhv)
+	public static function isVectorEqual($lhv, $rhv)
 	{
-		if (UnityEngine.Mathf.Abs(lhv.x - rhv.x) < 0.0001f)
+		if (UnityEngine.Mathf.Abs(lhv.x - rhv.x) < 0.0001)
 		{
-			if (UnityEngine.Mathf.Abs(lhv.y - rhv.y) < 0.0001f)
+			if (UnityEngine.Mathf.Abs(lhv.y - rhv.y) < 0.0001)
 			{
-				if (UnityEngine.Mathf.Abs(lhv.z - rhv.z) < 0.0001f)
+				if (UnityEngine.Mathf.Abs(lhv.z - rhv.z) < 0.0001)
 				{
 					return true;
 				}
@@ -93,100 +93,100 @@ class UtilSysLibWrap
 	 */
 	public static function setTimeZone($timeZone = 'Asia/Shanghai')
 	{
-		date_default_timezone_set($timeZone)
+		date_default_timezone_set($timeZone);
 	}
 
-	public static int Range(int min, int max)
+	public static function Range($min, $max)
 	{
 		UnityEngine.Random.InitState((int)UtilSysLibWrap.getUTCSec());
 		return UnityEngine.Random.Range(min, max);
 	}
 
 	// 欧拉角增加
-	static public float incEulerAngles(float degree, float delta)
+	static public function incEulerAngles($degree, $delta)
 	{
 		return (degree + delta) % 360;
 	}
 
-	static public float decEulerAngles(float degree, float delta)
+	static public function decEulerAngles($degree, $delta)
 	{
 		return (degree - delta) % 360;
 	}
 
-	static public void assert(bool condition, string message = "")
+	static public function assert($condition, $message = "")
 	{
 		Debug.Assert(condition, message);
 	}
 
-	static public float rangRandom(float min, float max)
+	static public function rangRandom($min, $max)
 	{
 		return UnityEngine.Random.Range(min, max);
 	}
 
-	static public System.Text.Encoding convGkEncode2EncodingEncoding(GkEncode gkEncode)
+	static public function convGkEncode2EncodingEncoding($gkEncode)
 	{
-		System.Text.Encoding retEncode = System.Text.Encoding.UTF8;
+		$retEncode = System.Text.Encoding.UTF8;
 
 		if (GkEncode.eUTF8 == gkEncode)
 		{
-			retEncode = System.Text.Encoding.UTF8;
+			$retEncode = System.Text.Encoding.UTF8;
 		}
 		else if (GkEncode.eGB2312 == gkEncode)
 		{
-			retEncode = System.Text.Encoding.UTF8;
+			$retEncode = System.Text.Encoding.UTF8;
 		}
 		else if (GkEncode.eUnicode == gkEncode)
 		{
-			retEncode = System.Text.Encoding.Unicode;
+			$retEncode = System.Text.Encoding.Unicode;
 		}
 		else if (GkEncode.eDefault == gkEncode)
 		{
-			retEncode = System.Text.Encoding.Default;
+			//$retEncode = System.Text.Encoding.Default;
 		}
 
-		return retEncode;
+		return $retEncode;
 	}
 
 	// 转换资源目录到精灵名字
-	static public string convFullPath2SpriteName(string fullPath)
+	static public function convFullPath2SpriteName($fullPath)
 	{
-		string spriteName = "";
-		int lastSlashIndex = -1;
-		int dotIndex = -1;
+		$spriteName = "";
+		$lastSlashIndex = -1;
+		$dotIndex = -1;
 
-		lastSlashIndex = UtilStr.LastIndexOf(ref fullPath, '/');
+		$lastSlashIndex = UtilStr.LastIndexOf($fullPath, '/');
 
-		if(-1 == lastSlashIndex)
+		if(-1 == $lastSlashIndex)
 		{
-			lastSlashIndex = UtilStr.LastIndexOf(ref fullPath, '\\');
+			$lastSlashIndex = UtilStr.LastIndexOf($fullPath, '\\');
 		}
 
-		dotIndex = UtilStr.LastIndexOf(ref fullPath, '.');
+		$dotIndex = UtilStr.LastIndexOf($fullPath, '.');
 		
-		if (-1 == lastSlashIndex)
+		if (-1 == $lastSlashIndex)
 		{
-			if (-1 == dotIndex)
+			if (-1 == $dotIndex)
 			{
-				spriteName = fullPath;
+				$spriteName = $fullPath;
 			}
 			else
 			{
-				spriteName = fullPath.Substring(0, dotIndex);
+				$spriteName = $fullPath.Substring(0, $dotIndex);
 			}
 		}
 		else
 		{
-			if (-1 == dotIndex)
+			if (-1 == $dotIndex)
 			{
-				spriteName = fullPath.Substring(lastSlashIndex + 1, fullPath.Length - lastSlashIndex);
+				$spriteName = $fullPath.Substring($lastSlashIndex + 1, $fullPath.Length - $lastSlashIndex);
 			}
 			else
 			{
-				spriteName = fullPath.Substring(lastSlashIndex + 1, dotIndex - (lastSlashIndex + 1));
+				$spriteName = $fullPath.Substring($lastSlashIndex + 1, $dotIndex - ($lastSlashIndex + 1));
 			}
 		}
 
-		return spriteName;
+		return $spriteName;
 	}
 }
 

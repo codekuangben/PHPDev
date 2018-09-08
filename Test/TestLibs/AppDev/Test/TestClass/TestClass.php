0719ec5f95845118a4ec21fc94ef9f4a52bcf169
codekuangben <kuangben2001@163.com>
2018-09-09 00:26:28 +0800

    aaa

--- Test/TestLibs/AppDev/Test/TestClass/TestClass.php
+++ Test/TestLibs/AppDev/Test/TestClass/TestClass.php
@@ -4,6 +4,13 @@ namespace SDK\Test;
 
 class TestClass extends TestBase
 {
+    // 静态成员变量名字必须 $ 开始
+    protected static $TestStaticMember;
+    // 常量成员变量名字不能以 $ 开始,并且需要赋值初始值
+    protected const TestConstMember = 0;
+    // 成员变量名字必须 $ 开始
+    protected $TestMember;
+    
     public function __construct()
     {
         parent::__construct();
